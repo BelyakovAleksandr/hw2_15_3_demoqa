08d2148485ab92af86c108f37229dbbde95c6d99
BelyakovAleksandr <113695437+BelyakovAleksandr@users.noreply.github.com>
2022-11-27 18:45:18 +0400

    15_3_demoqa_Sashka Update

---
+++ src/test/java/tests/PracticeFormPageObjectsTests.java
@@ -0,0 +1,70 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pages.PracticeFormPageObjectsPage;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class PracticeFormPageObjectsTests {
+
+    PracticeFormPageObjectsPage practiceFormPageObjectsPage = new PracticeFormPageObjectsPage();
+
+    @BeforeAll
+    static void configuration() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1700x800";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillFormTest() throws IOException, URISyntaxException {
+        practiceFormPageObjectsPage.openPage()
+                .setFirstName("Egor")
+                .setLastName("Krid")
+                .setEmail("ekrid@test.com")
+                .setGender("Female")
+                .setNumber("1234567890")
+                .setBirthdate("1990","November","29")
+                .setSubjects("Maths")
+                .setHobbies("Sports")
+                .setHobbies("Reading")
+                .setHobbies("Music")
+                .uploadPicture("https://demoqa.com/images/Test_Demo.jpg")
+                .setAddress("Orenburg")
+                .setState("Haryana")
+                .setCity("Karnal")
+                .clickSubmit()
+
+                .checkResultsVisible()
+                .checkResult("Student Name", "Egor Krid")
+                .checkResult("Student Email", "ekrid@test.com")
+                .checkResult("Gender", "Female")
+                .checkResult("Mobile", "1234567890")
+                .checkResult("Date of Birth", "29 November,1990")
+                .checkResult("Subjects", "Maths")
+                .checkResult("Hobbies", "Sports, Reading, Music")
+                .checkResult("Picture", "Test_Demo.jpg")
+                .checkResult("Address", "Orenburg")
+                .checkResult("State and City", "Haryana Karnal")
+                .closeResultModal();
+    }
+
+    @Test
+    void fillFormWithMinimumDataTest() {
+        practiceFormPageObjectsPage.openPage()
+                .setFirstName("Egor")
+                .setLastName("Krid")
+                .setGender("Female")
+                .setNumber("1234567890")
+                .clickSubmit()
+
+                .checkResultsVisible()
+                .checkResult("Student Name", "Egor Krid")
+                .checkResult("Gender", "Female")
+                .checkResult("Mobile", "1234567890")
+                .closeResultModal();
+    }
+}
---
+++ src/test/java/tests/PracticeFormTests.java
@@ -0,0 +1,73 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeFormTests {
+
+    @BeforeAll
+    static void configuration() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1700x800";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillFormTest() throws IOException, URISyntaxException {
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#firstName").setValue("Aleksandr");
+        $("#lastName").setValue("Pushkin");
+        $("#userEmail").setValue("apushkin@test.com");
+
+        $(byText("Female")).click();
+
+        $("#userNumber").setValue("1234567890");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("November");
+        $(".react-datepicker__year-select").selectOption("1990");
+        $("[aria-label='Choose Thursday, November 29th, 1990']").click();
+
+        $("#subjectsInput").setValue("Maths").pressEnter();
+
+        $(byText("Sports")).click();
+        $(byText("Reading")).click();
+        $(byText("Music")).click();
+
+
+        $("#uploadPicture").uploadFile(download("https://demoqa.com/images/Toolsqa.jpg"));
+
+        $("#currentAddress").setValue("bla bla bla");
+
+        $("#state").click();
+        $(byText("Haryana")).click();
+        $("#city").click();
+        $(byText("Karnal")).click();
+
+        $("#submit").pressEnter();
+
+        $(".modal-header").shouldHave(Condition.text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(Condition.text("Aleksandr Pushkin"),
+                Condition.text("test@test.com"),
+                Condition.text("Female"),
+                Condition.text("1234567890"),
+                Condition.text("29 November,1990"),
+                Condition.text("Maths"),
+                Condition.text("Sports, Reading, Music"),
+                Condition.text("Test_Demo.jpg"),
+                Condition.text("bla bla bla"),
+                Condition.text("Haryana Karnal"));
+        $("#closeLargeModal").click();
+    }
+}
